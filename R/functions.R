ac257845042a347705479568d17eafced75d63e5
AndersTFrederiksen <AndersTFrederiksen@gmail.com>
2025-12-03 10:16:42 +0100

    old commits

--- R/functions.R
+++ R/functions.R
@@ -5,8 +5,8 @@
 #' @returns A data.frame
 #'
 create_table_descriptive_stats <- function(data) {
-  data %>%
-    dplyr::group_by(metabolite) %>%
+  data |>
+    dplyr::group_by(metabolite) |>
     dplyr::summarise(
       dplyr::across(
         value,
@@ -15,11 +15,11 @@ create_table_descriptive_stats <- function(data) {
           sd = sd
         )
       )
-    ) %>%
+    ) |>
     dplyr::mutate(dplyr::across(
       tidyselect::where(is.numeric),
       \(x) round(x, digits = 2)
-    )) %>%
-    dplyr::mutate(MeanSD = glue::glue("{value_mean} ({value_sd})")) %>%
+    )) |>
+    dplyr::mutate(MeanSD = glue::glue("{value_mean} ({value_sd})")) |>
     dplyr::select(Metabolite = metabolite, "Mean SD" = MeanSD)
 }
