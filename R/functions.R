b7517be15e30faf7357e8aba7ca0246883690512
AndersTFrederiksen <AndersTFrederiksen@gmail.com>
2025-12-03 11:07:25 +0100

    Create plot distributions funktion i R-functions

--- R/functions.R
+++ R/functions.R
@@ -34,9 +34,10 @@ create_table_descriptive_stats <- function(data) {
 create_plot_distributions <- function(data) {
   ggplot2::ggplot(
     data,
-    aes(x = as.numeric(value))
+    ggplot2::aes(x = value)
   ) +
     ggplot2::geom_histogram() +
-    ggplot2::facet_wrap(vars(metabolite), scales = "free") +
+    ggplot2::facet_wrap(ggplot2::vars(metabolite), scales = "free") +
     ggplot2::theme_minimal()
 }
+
