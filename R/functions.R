fad92765f138c8c4f848408ddf0abff8ba72e3ea
AndersTFrederiksen <AndersTFrederiksen@gmail.com>
2025-12-03 11:00:08 +0100

    Putting in the ggplot as a function

--- R/functions.R
+++ R/functions.R
@@ -23,3 +23,20 @@ create_table_descriptive_stats <- function(data) {
     dplyr::mutate(MeanSD = glue::glue("{value_mean} ({value_sd})")) |>
     dplyr::select(Metabolite = metabolite, "Mean SD" = MeanSD)
 }
+
+
+#' Title: GGplot of histogram distributions of values
+#'
+#' @param data
+#'
+#' @returns A plot object
+#'
+create_plot_distributions <- function(data) {
+  ggplot2::ggplot(
+    data,
+    aes(x = as.numeric(value))
+  ) +
+    ggplot2::geom_histogram() +
+    ggplot2::facet_wrap(vars(metabolite), scales = "free") +
+    ggplot2::theme_minimal()
+}
